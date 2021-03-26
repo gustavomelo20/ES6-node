16f12aa8452916b11859110f72c27f501627c65d
gustavomelo20 <infogustavomelo@gmail.com>
2021-03-26 01:04:01 -0300

    exports

--- modulo/app.js
+++ modulo/app.js
@@ -1,3 +1,8 @@
 const mod1 = require('./mod1');
 const falaNome = mod1.falaNome;
 console.log(falaNome());
+
+
+const {Pessoa} = require('./pessoa');
+const p1 = new Pessoa('Carolina');
+console.log(p1)
